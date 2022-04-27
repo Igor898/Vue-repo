98d0082d385879d4645683081caf011c055d7d62
Igor <potemkin_2002@bk.ru>
2022-04-27 21:42:32 +0300

    files of 3 lesson

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <header>
+      <h1>{{ msg }}</h1>
+    </header>
+    <main>
+      <payments-display :items="paymentsList"/>
+      <add-payment-form @addPayment="AddPaymentData"/>
+    </main>
+  </div>
+</template>
+
+<script>
+import AddPaymentForm from './AddPaymentForm.vue'
+import PaymentsDisplay from './PaymentsDisplay.vue'
+
+export default {
+  components: { PaymentsDisplay, AddPaymentForm },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }, 
+  data() {
+    return {
+      paymentsList: []
+    }
+  },
+  methods: {
+    AddPaymentData(data) {
+      this.paymentsList.push(data)
+    },
+    fetchData() {
+      return [
+        {
+          date: '28.03.2020',
+          category: 'Food',
+          value: 169,
+        },
+        {
+          date: '24.03.2020',
+          category: 'Transport',
+          value: 360,
+        },
+        {
+          date: '24.03.2020',
+          category: 'Food',
+          value: 532,
+        },
+      ]     
+    }
+  },
+  created() {
+    this.paymentsList = this.fetchData()
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
